1ca489b95b60e042340e7ffc8b6f7fb431f02d3f
Shreeraam23 <161189756+Shreeraam23@users.noreply.github.com>
2025-04-04 15:27:21 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -17,11 +17,11 @@ if ENVIRONMENT:
         MUST_JOIN = MUST_JOIN.replace("@", "")
 else:
     # Fill the Values
-    API_ID = 0
-    API_HASH = ""
-    BOT_TOKEN = ""
-    DATABASE_URL = ""
-    DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")
-    MUST_JOIN = ""
+    API_ID = 28036362
+    API_HASH = "84e4bd8457dab2f065801bde8874842a"
+    BOT_TOKEN = "8189822314:AAGpd4guRLI1zRskAjIk5Dh6ERcfcFviiJU"
+    DATABASE_URL = "mongodb+srv://dafeh64648:dxkiXApD489QKbjN@ramsingh.o0kha.mongodb.net/?retryWrites=true&w=majority&appName=Ramsingh"
+    DATABASE_URL = DATABASE_URL.replace("postgres", "Ramsingh")
+    MUST_JOIN = "myserver23"
     if MUST_JOIN.startswith("@"):
         MUST_JOIN = MUST_JOIN[1:]
